5f52bef173b22ebf52a5f69f1626cf8f5cd00746
Jean Santos <jeansantosc@gmail.com>
2018-08-19 17:58:52 -0300

    rendering dummy data with a list

--- src/App.js
+++ src/App.js
@@ -2,19 +2,44 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const LIST = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to
-          reload.
-        </p>
-      </div>
+      <ul className="App">
+        {LIST.map(item => {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url} title={item.title}>
+                  {item.title}
+                </a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     );
   }
 }
