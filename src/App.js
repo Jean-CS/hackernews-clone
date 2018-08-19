4004b6c1213119a9827f435268859230aea0b4ba
Jean Santos <jeansantosc@gmail.com>
2018-08-19 18:01:33 -0300

    concise body arrow function

--- src/App.js
+++ src/App.js
@@ -25,20 +25,18 @@ class App extends Component {
   render() {
     return (
       <ul className="App">
-        {LIST.map(item => {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url} title={item.title}>
-                  {item.title}
-                </a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
+        {LIST.map(item => (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url} title={item.title}>
+                {item.title}
+              </a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        ))}
       </ul>
     );
   }
