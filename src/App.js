793d1980463b1368d22ac478c02deb8842c84b16
Jean Santos <jeansantosc@gmail.com>
2018-09-01 23:00:32 -0300

    implemented error handling

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ class App extends Component {
       searchKey: '',
       // cant be used for the above because it changes everytime you type in the search field
       searchTerm: DEFAULT_QUERY,
+      error: null,
     };
 
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(
@@ -49,7 +50,7 @@ class App extends Component {
     fetch(apiUrl)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+      .catch(error => this.setState({ error }));
   }
 
   setSearchTopStories(result) {
@@ -105,7 +106,7 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, results, searchKey } = this.state;
+    const { searchTerm, results, searchKey, error } = this.state;
     const page =
       (results && results[searchKey] && results[searchKey].page) || 0;
     const list =
@@ -124,7 +125,14 @@ class App extends Component {
           </Search>
         </div>
 
-        <Table list={list} onDismiss={this.onDismiss} />
+        {error ? (
+          <div className="interactions">
+            <p>Something went wrong.</p>
+            {console.log(error)}
+          </div>
+        ) : (
+          <Table list={list} onDismiss={this.onDismiss} />
+        )}
 
         <div className="interactions">
           <Button
